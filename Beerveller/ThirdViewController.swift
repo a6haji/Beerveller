e224e7932007311e01eb48816e1ee6b650abe3ec
a6haji <aly.haji99@gmail.com>
2016-07-08 00:01:58 -0400

    Added logic for default text button

--- Beerveller/ThirdViewController.swift
+++ Beerveller/ThirdViewController.swift
@@ -10,26 +10,24 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    
+    //MARK: Properties
+    @IBOutlet weak var beerNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Actions
+    @IBAction func setDefaultTextLabel(sender: UIButton) {
+        
+        beerNameLabel.text = "Default Text"
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
